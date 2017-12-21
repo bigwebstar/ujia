79ce1407e2c0a018248ff7a37885d0275904d3ba
sunyadv <976575821@qq.com>
2017-12-21 09:04:45 +0800

    zufang

--- publish-rent/js/publish-rent-more.js
+++ publish-rent/js/publish-rent-more.js
@@ -2,47 +2,21 @@ $(()=>{
     $("#detailsSubmit").click(function(e){
         e.preventDefault();
         var title = $("#title").val();
-        console.log(title);
-
         var area = $("#area").val();
-        console.log(area);
-
         var district = $("#district").val();
-        console.log(district);
-
         var metro = $("#metro").val();
-        console.log(metro);
-
         var buildingNo = $("#buildingNo").val();
-        console.log(buildingNo);
-
         var price = $("#price").val();
-        console.log(price);
-
         var size = $("#size").val();
-        console.log(size);
-
         var house_room = $("#house_room").find("option:selected").text();
-        console.log(house_room);
-
         var house_hall = $("#house_hall").find("option:selected").text();
-        console.log(house_hall);
-
         var house_kitchen = $("#house_kitchen").find("option:selected").text();
-        console.log(house_kitchen);
-
         var house_toilet = $("#house_toilet").find("option:selected").text();
-        console.log(house_toilet);
-
         var side = $("#side").find("option:selected").text();
-        console.log(side);
-
         var floor = $("#floor").find("option:selected").text();
         var floorNum = $("#floorNum").val();
 
         floor = floor + "(共"+floorNum+"层)";
-        console.log(floor);
-
         $.ajax({
             url:"data/zufang_publish.php",
             type:"POST",
